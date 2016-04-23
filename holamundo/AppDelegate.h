3ec1e142ef011256a169758a22e81f75a583c538
marcos rossi <rma3000@gmail.com>
2016-04-23 19:19:21 -0300

    Initial Commit

---
+++ holamundo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  holamundo
+//
+//  Created by Administrator on 4/23/16.
+//  Copyright © 2016 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ holamundo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  holamundo
+//
+//  Created by Administrator on 4/23/16.
+//  Copyright © 2016 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
